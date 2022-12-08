a526e0ed9f5d4753c07145d05f4c19fc2bfcab53
alany1 <alanyu@csail.mit.edu>
2022-12-08 01:48:27 -0500

    add extension'

--- generate.py
+++ generate.py
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     
     sustainer = {'Chord': sustainer[0], 'Span': sustainer[1], 'SweepDistance': sustainer[2] , 'TipChord': sustainer[3]}
     booster = {'Chord': booster[0], 'Span': booster[1], 'SweepDistance': booster[2] , 'TipChord': booster[3]}
-    fname = f"samples/new_{''.join([str(x) for x in args.floats])}"
+    fname = f"samples/new_{''.join([str(x) for x in args.floats])}.CDX1"
     generateNewDesign(booster, sustainer, fname)
     print(f'Done generating file {fname}.')
